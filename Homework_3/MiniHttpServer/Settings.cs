87184eca58a2bb8b6a5de684d81ba1f3c19fc6b7
Quartrxng <>
2025-10-10 16:36:52 +0300

    HomeWork 3
    Было сделан Singleton, вынесена логика из ListenerCallback, улчушено чтение файлов

---
+++ Homework_3/MiniHttpServer/Settings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer
+{
+    public sealed class Settings
+    {
+        private static Settings _instance;
+        public static Settings Instance()
+        {
+            if (_instance == null)
+                _instance = new Settings();
+            return _instance;
+        }
+
+        public string PublicDirectoryPath { get; private set; }
+        public string Domain { get; private set; }
+        public string Port { get; private set; }
+
+        private Settings()
+        {
+            try
+            {
+                var json = File.ReadAllText("settings.json");
+                var settings = JsonSerializer.Deserialize<Settings>(json);
+                PublicDirectoryPath = settings.PublicDirectoryPath;
+                Domain = settings.Domain;
+                Port = settings.Port;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Файл settings.json не найден");
+                PublicDirectoryPath = "public/";
+                Domain = "localhost";
+                Port = "1337";
+            }
+            catch (JsonException)
+            {
+                Console.WriteLine("Ошибка формата JSON");
+                PublicDirectoryPath = "public/";
+                Domain = "localhost";
+                Port = "1337";
+            }
+            catch
+            {
+                PublicDirectoryPath = "public/";
+                Domain = "localhost";
+                Port = "1337";
+            }
+        }
+    }
+}
---
+++ Homework_3/MiniHttpServer/Shared/Contoller.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniHttpServer.Shared
+{
+    public class GetResponse
+    {
+        public static byte[] GetBytes(string path)
+        {
+                byte[] answer = File.ReadAllBytes(path);
+                return answer;
+        }
+    }
+
+    public class GetPath
+    {
+        public static string Path(HttpListenerRequest? request, string publicDirectoryPath)
+        {
+            var decodedPath = Uri.UnescapeDataString(request.Url.AbsolutePath);
+            var path = decodedPath[^1] == '/' ? decodedPath + "index.html" : decodedPath;
+            return publicDirectoryPath + path;
+        }
+    }
+}
